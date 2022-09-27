f07bab3ae5d647dc3c44816f6b3b2940d2b34c56
Matti Stenvall <matti.stenvall@gmail.com>
2022-09-27 12:48:12 +0300

    Basic sensor setup done

--- app/src/main/java/com/jasminsp/weatherapp/MainActivity.kt
+++ app/src/main/java/com/jasminsp/weatherapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.jasminsp.weatherapp
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,14 +23,18 @@ import androidx.lifecycle.MutableLiveData
 import com.jasminsp.weatherapp.ui.theme.WeatherAppTheme
 import com.jasminsp.weatherapp.weather.WeatherViewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(), SensorEventListener {
     companion object {
         private lateinit var weatherViewModel: WeatherViewModel
+        private lateinit var sensorManager: SensorManager
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpSensor()
         setContent {
             weatherViewModel = WeatherViewModel(application)
+            sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
             WeatherAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -35,6 +45,38 @@ class MainActivity : ComponentActivity() {
                     //weatherViewModel.getFavouriteWeather(52.52437, 13.41053)
                 }
             }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+        return
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        // TODO: update an observed variable when the received sensor value changes
+        if (event.sensor?.type == Sensor.TYPE_PRESSURE) {
+            Log.i("SENSOR", event.values[0].toString())
+        }
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        setUpSensor()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // unregister the listener when activity pauses to save battery, change if want to work in background
+        sensorManager.unregisterListener(this)
+    }
+
+    private fun setUpSensor() {
+        Log.i("SENSOR", "Setup temp sensor started")
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)?.also {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_NORMAL)
         }
     }
 }
---
+++ app/src/main/java/com/jasminsp/weatherapp/sensor/SensorViewModel.kt
@@ -0,0 +1,8 @@
+package com.jasminsp.weatherapp.sensor
+
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+
+class SensorViewModel : ViewModel() {
+
+}
