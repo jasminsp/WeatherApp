72e040a4a65d192afd148d0c87e064b68f95c0e9
jasminsp <74709722+jasminsp@users.noreply.github.com>
2022-09-29 19:24:51 +0300

    styling search view

--- app/src/main/java/com/jasminsp/weatherapp/MainActivity.kt
+++ app/src/main/java/com/jasminsp/weatherapp/MainActivity.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -14,7 +14,13 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jasminsp.weatherapp.ui.theme.WeatherAppTheme
 import com.jasminsp.weatherapp.weather.WeatherViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -53,13 +59,32 @@ class MainActivity : ComponentActivity() {
     fun ShowSearchResult(viewModel: WeatherViewModel) {
         val searchResult by viewModel.searchedLocations.observeAsState()
 
-        LazyColumn {
-            searchResult?.results?.forEach {
-                item {
-                    Card(Modifier.clickable {
-                        onSearchResultClick(viewModel, it.latitude, it.longitude)
-                    }) {
-                        it.country?.let { name -> Text(name) }
+        Column(Modifier.fillMaxWidth()) {
+            // Place for search input
+            LazyColumn(Modifier.fillMaxWidth()) {
+                searchResult?.results?.forEach {
+                    item {
+                        Card(
+                            Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    onSearchResultClick(viewModel, it.latitude, it.longitude)
+                                }) {
+                            Column(Modifier.padding(all = 20.dp)) {
+                                Row(Modifier.fillMaxWidth().padding(start = 15.dp), Arrangement.SpaceBetween, Alignment.CenterVertically) {
+                                    if (it.admin3 != null) Text(
+                                        it.admin3,
+                                        fontSize = 22.sp
+                                    ) else it.admin2?.let { name -> Text(name, fontSize = 22.sp) }
+                                    Text("Add", Modifier.padding(top = 20.dp), color = Color.Gray)
+                                }
+                                Text(
+                                    "${it.admin1}, ${it.country}",
+                                    Modifier.padding(start = 15.dp),
+                                    fontWeight = FontWeight(400), color = Color.Gray
+                                )
+                            }
+                        }
                     }
                 }
             }
